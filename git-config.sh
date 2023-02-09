ab0e88b2bf81d0fd14cd0e33b1287cfb6bf0a081
Matthias Budde <buddemat@users.noreply.github.com>
2023-02-09 22:28:36 +0100

    Feat: Added git configuration script.

---
+++ git-config.sh
@@ -0,0 +1,13 @@
+git config user.name "Matthias Budde"
+git config user.email "buddemat@users.noreply.github.com"
+git config --global alias.stat status
+git config --global alias.ci commit
+git config --global init.defaultBranch main
+git config --global pull.rebase true
+git config --global fetch.prune true
+git config --global diff.colorMoved zebra
+git config --global mergetool.prompt false
+git config --global merge.conflictstyle diff3
+git config --global merge.tool vimdiff
+git config --global pager.log false
+git config --global core.editor vim
