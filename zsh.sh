e4b5616d44293d1e34b69067990b9db2a56624cf
Matthias Budde <buddemat@users.noreply.github.com>
2023-02-13 21:50:00 +0100

    Feat: Added zsh plugin fzf.

--- zsh.sh
+++ zsh.sh
@@ -1,11 +1,27 @@
-sudo apt install fonts-powerline -y
+# install packages needed for later config/plugins
+sudo apt-get install fonts-powerline -y
+sudo apt-get install ripgrep -y
+sudo apt-get install fzf -y
 sudo apt-get install zsh -y
-sudo chsh -s /usr/bin/zsh $USER # change default shell to zsh
+
+# change default shell to zsh
+sudo chsh -s /usr/bin/zsh $USER 
 touch ~/.zshrc
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # set theme
 sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="agnoster"/' ~/.zshrc
+# enable auto update
+sed -i "s/^# \(zstyle ':omz:update' mode auto\)/\1/" ~/.zshrc
+
+# enable plugin fzf
+sed -i '/^plugins=(.*)$/aexport DISABLE_FZF_AUTO_COMPLETION="true"' ~/.zshrc
+sed -i '/^plugins=(.*)$/aexport DISABLE_FZF_KEY_BINDINGS="true"' ~/.zshrc
+sed -i "/^plugins=(.*)$/aexport FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'" ~/.zshrc
+sed -i "/^plugins=(.*)$/aexport FZF_BASE=$(which fzf)" ~/.zshrc
+sed -i "/^source \$ZSH\/oh-my-zsh.sh/asource /usr/share/doc/fzf/examples/completion.zsh" ~/.zshrc
+sed -i "/^source \$ZSH\/oh-my-zsh.sh/asource /usr/share/doc/fzf/examples/key-bindings.zsh" ~/.zshrc
 
 # install and enable plugin zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
