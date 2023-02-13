11d1e81b5ce0b8c30d9ea8b7b897b759f93ff694
Matthias Budde <buddemat@users.noreply.github.com>
2023-02-13 15:03:07 +0100

    Feat: Added basic zsh installation script.

---
+++ zsh.sh
@@ -0,0 +1,6 @@
+sudo apt-get install zsh -y
+sudo chsh -s /usr/bin/zsh $USER # change default shell to zsh
+touch ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="agnoster"/' ~/.zshrc
+
