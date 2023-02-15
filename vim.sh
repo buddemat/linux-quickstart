11c8fc612ec83bdfad08e1888c8b08f3a64ed545
Matthias Budde <buddemat@users.noreply.github.com>
2023-02-15 23:48:49 +0100

    Feat: Vim script checks out vimrc now.

--- vim.sh
+++ vim.sh
@@ -1,2 +1,14 @@
+# install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# checkout vimrc to temporary file
+FILE="vimrc-$(date +"%Y-%m-%d_%H%M%S")"
+# get basic vimrc from github
+wget https://raw.githubusercontent.com/buddemat/vimrc/main/vimrc  --output-document=$FILE
+# backup old vimrc, if exists
+mv --backup=t $FILE ~/.vimrc
+
+
+# install vimplugins, see https://github.com/junegunn/vim-plug
+vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
