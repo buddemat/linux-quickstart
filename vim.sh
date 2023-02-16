be9e24dea0a7117c29a072443ed31e285629eb89
Matthias Budde <buddemat@users.noreply.github.com>
2023-02-16 21:49:11 +0100

    Feat: Added pulling vim-plug source file.

--- vim.sh
+++ vim.sh
@@ -6,12 +6,21 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # checkout vimrc to temporary file
-FILE="vimrc-$(date +"%Y-%m-%d_%H%M%S")"
-# get basic vimrc from github
-wget https://raw.githubusercontent.com/buddemat/vimrc/main/vimrc  --output-document=$FILE
-# backup old vimrc, if exists
-mv --backup=t $FILE ~/.vimrc
+TEMPDIR="vimrc-$(date +"%Y-%m-%d_%H%M%S")"
+mkdir $TEMPDIR
 
+# get basic vimrc and vimplug section from github
+wget https://raw.githubusercontent.com/buddemat/vimrc/main/vimrc --output-document=$TEMPDIR/vimrc
+wget https://raw.githubusercontent.com/buddemat/vimrc/main/vimplug --output-document=$TEMPDIR/vimplug
+
+# concat the two
+cat $TEMPDIR/vimplug $TEMPDIR/vimrc >> $TEMPDIR/vimcat
+    
+# backup old vimrc, if exists
+mv --backup=t $TEMPDIR/vimcat ~/.vimrc
 
 # install vimplugins, see https://github.com/junegunn/vim-plug
-vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
+vim -es -u ~/.vimrc -i NONE -c "PlugInstall" -c "qa"
+
+# clean up
+rm -r $TEMPDIR
