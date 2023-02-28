c60de31c857fa5a2beba3a9a72c0d0ca49d74a75
Matthias Budde <buddemat@users.noreply.github.com>
2023-02-28 16:50:17 +0100

    Feat: Added flatpack installaton packages.

--- applications.sh
+++ applications.sh
@@ -4,3 +4,14 @@ sudo apt-get install gimp -y
 
 # QGIS, installs a lot of dependencies
 sudo apt-get install qgis -y
+
+## Flatpack
+
+#Zoom
+flatpak install flathub us.zoom.Zoom
+
+# MS Teams
+flatpak install flathub com.microsoft.Teams
+
+# RocketChat
+flatpak install flathub chat.rocket.RocketChat
