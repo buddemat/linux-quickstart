6b02ae9f3d328af89160c16066bd8b660358963e
Matthias Budde <buddemat@users.noreply.github.com>
2023-03-14 14:52:35 +0100

    Feat: Added dark theme exception for libreoffice.

--- gtk-config.sh
+++ gtk-config.sh
@@ -4,3 +4,11 @@
 # Thunderbird
 cp /usr/share/applications/thunderbird.desktop ~/.local/share/applications
 sed -i 's/^Exec=\(.*\)$/Exec=env GTK_THEME=Mint-Y-Aqua \1/' ~/.local/share/applications/thunderbird.desktop
+
+# Libre Office (all)
+for fname in /usr/share/applications/libreoffice-*.desktop; do
+    cp $fname ~/.local/share/applications
+    sed -i 's/^Exec=\(.*\)$/Exec=env GTK_THEME=Mint-Y-Aqua \1/' $fname
+done
+
+
