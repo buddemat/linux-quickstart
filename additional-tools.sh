7c7b4db1618eaf2369ea53a2bfe4dad2da227d56
Matthias Budde <buddemat@users.noreply.github.com>
2023-11-28 22:50:22 +0100

    Feat: Added kubectl installation.

--- additional-tools.sh
+++ additional-tools.sh
@@ -25,3 +25,10 @@ sudo apt-get install -y nginx
 
 # GDAL
 sudo apt-get install -y libgdal-dev
+
+# kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl kubectl.sha256
