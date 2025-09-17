377ba633302ca7fb0b55a39f3d3a19428edbd43c
Matthias Budde <buddemat@users.noreply.github.com>
2025-09-17 11:51:26 +0200

    Feat: Added dive container inspection tool.

--- docker.sh
+++ docker.sh
@@ -38,3 +38,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
 
+# install dive (container inspection tool https://github.com/wagoodman/dive)
+DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+curl -fOL "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb"
+sudo apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
+
