4c7734a722264852d6c2f73bdd4c1f37c54201b2
Matthias Budde <buddemat@users.noreply.github.com>
2026-09-18 12:10:48 +0200

    Feat: Added trivy.

--- docker.sh
+++ docker.sh
@@ -59,3 +59,8 @@ echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.co
 sudo apt-get update
 sudo apt-get install helm
 
+# trivy for image CVE inspection
+curl -fsSL https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
